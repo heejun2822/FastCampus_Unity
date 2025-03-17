3610b585ebc43cc55b2dfd833a2e2162270dcc48
heejun2822 <godfam44@gmail.com>
2025-03-17 23:49:02 +0900

    [VSL] game object management

--- VAMserLike/Assets/Script/Manager/GamePoolManager.cs
+++ VAMserLike/Assets/Script/Manager/GamePoolManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,16 +16,44 @@ public class GamePoolManager
             return sInstance;
         }
     }
-
     public void Init()
     {
-
+        SkillPool = new Dictionary<SkillType, Queue<SkillBase>>();
     }
 
     public void Clear()
     {
-        
+        SkillPool.Clear();
+        SkillPool = null;
+    }
+
+    public void EnqueueSkillPool(SkillBase InSkill)
+    {
+        if (SkillPool == null)
+        {
+            return;
+        }
+        if (SkillPool.ContainsKey(InSkill.mSkillType) == false)
+        {
+            SkillPool.Add(InSkill.mSkillType, new Queue<SkillBase>());
+        }
+        SkillPool[InSkill.mSkillType].Enqueue(InSkill);
+    }
+
+    public SkillBase DequeueSkillPool(SkillType InSkillType)
+    {
+        if (SkillPool == null)
+        {
+            return null;
+        }
+        if (SkillPool.ContainsKey(InSkillType) == false)
+        {
+            return null;
+        }
+        return SkillPool[InSkillType].Dequeue();
     }
 
     private static GamePoolManager sInstance = null;
+
+    private Dictionary<SkillType, Queue<SkillBase>> SkillPool = null;
 }
