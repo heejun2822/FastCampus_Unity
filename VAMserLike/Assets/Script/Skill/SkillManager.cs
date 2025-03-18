8dcd771b14f7c7d02cec24ac36b680a74b1727ed
heejun2822 <godfam44@gmail.com>
2025-03-19 01:56:43 +0900

    [VSL] individual skill implementation

--- VAMserLike/Assets/Script/Skill/SkillManager.cs
+++ VAMserLike/Assets/Script/Skill/SkillManager.cs
@@ -16,7 +16,7 @@ public class SkillManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CurrentCooltime += Time.deltaTime;
     }
 
     private void _OnMouseInput(int InIndex, Vector3 InMousePos)
@@ -24,21 +24,34 @@ public class SkillManager : MonoBehaviour
         RaycastHit IHit;
         Ray IRay = Camera.main.ScreenPointToRay(InMousePos);
         int layermask = 1 << LayerMask.NameToLayer("Terrain");
-
         if (Physics.Raycast(IRay, out IHit, 1000, layermask))
         {
             ActiveSkillData NewSkillData = new ActiveSkillData();
+            NewSkillData.Type = SkillType.ManualMissile;
             NewSkillData.FirePosition = IHit.point;
+            NewSkillData.Cooltime = 0.5f;
+            NewSkillData.Speed = 10.0f;
             FireSkill(NewSkillData);
         }
     }
 
     public void FireSkill(ActiveSkillData InSkillData)
     {
+        if (CurrentCooltime < InSkillData.Cooltime)
+        {
+            return;
+        }
+        MyPcUnitMovement IMovement = GetComponent<MyPcUnitMovement>();
+        if (IMovement != null)
+        {
+            IMovement.DoManualAttack(InSkillData.Type, InSkillData.FirePosition);
+        }
+
         Vector3 ShotDirection = (InSkillData.FirePosition - transform.position).normalized;
         Vector3 startPos = new Vector3(transform.position.x, 1, transform.position.z);
 
         FireSkillObject(InSkillData, startPos, ShotDirection);
+        CurrentCooltime = 0.0f;
     }
 
     public void FireSkillObject(ActiveSkillData InSkillData, Vector3 InStartPos, Vector3 InSkillDir)
@@ -52,7 +65,10 @@ public class SkillManager : MonoBehaviour
             {
                 return;
             }
-            SkillObject.transform.position = InStartPos;
         }
+        SkillObject.gameObject.SetActive(true);
+        SkillObject.FireSkill(InSkillData, InStartPos, InSkillDir);
     }
+
+    public float CurrentCooltime = 0.0f;
 }
